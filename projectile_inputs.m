619f3097dcb8f0dbb4719857102b9139611f63fe
jameslee124 <James@Jamess-MacBook-Pro-4.local>
2016-03-23 18:01:19 -0700

    more changes

--- projectile_inputs.m
+++ projectile_inputs.m
@@ -1,12 +1,12 @@
-function [v0,theta,t] = projectile_inputs
+function [ v_0,theta,t ] = projectile_inputs 
 %projectile_inputs takes user inputs of initial velocity, the angle of
 %departure and the instataneous time and puts it into a vector.
-
-v0 = input('Please input the initial velocity in [m/s]:\n');
+v_0 = input('Please input the initial velocity in [m/s]:\n');
 theta = input('Please input the angle of departure in [deg]:\n');
 t = input('Please input the instataneous time in [s]:\n');
-while v0 < 0 || theta < 0 || t < 0
-    v0 = input('Please reinput the non-negative initial velocity in [m/s]:\n');
+
+while v_0 < 0 || theta < 0 || t < 0
+    v_0 = input('Please reinput the non-negative initial velocity in [m/s]:\n');
     theta = input('Please reinput the non-negative angle of departure in [deg]:\n');
     t = input('Please reinput the non-negative instataneous time in [s]:\n');
 end
--- projectile_motion.m
+++ projectile_motion.m
@@ -1,15 +1,15 @@
-function [ x,y,tf,d,h ] = projectile_motion(v0,theta,t)
+function [ x,y,tf,d,h ] = projectile_motion (v_0,theta,t)
 %PROJECTILE_MOTION Using instantaneous velocity, angle of departure, and
 %time to output x position, y position, time final, distance traveled, and
 %maximum height. 
 
 g=9.81;
-x = v0*cosd(theta)*t;
-y = v0.*sind(theta).*t-(1/2.*g.*(t.^2));
-t_Vy = (v0.*sind(theta))./g;
-tf = (v0*sind(theta))/(1/2*g);
-d = v0.*cosd(theta).*tf;
-h = v0.*sind(theta).*t_Vy-(1/2.*g.*(t_Vy.^2));
+x = v_0*cosd(theta)*t;
+y = v_0.*sind(theta).*t-(1/2.*g.*(t.^2));
+t_Vy = (v_0.*sind(theta))./g;
+tf = (v_0*sind(theta))/(1/2*g);
+d = v_0.*cosd(theta).*tf;
+h = v_0.*sind(theta).*t_Vy-(1/2.*g.*(t_Vy.^2));
 if t>tf
     fprintf('Error the time range of this projectile is %.4f.\n',tf)
 else
@@ -18,5 +18,4 @@ else
     fprintf('The maximum height is %.4f.\n',h)
 end
 
-end
 
